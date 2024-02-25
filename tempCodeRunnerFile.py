a142b85aa31bf834d12f3b8f169994b2a8a8aabf
JustKiet <lunarthenerd@gmail.com>
2024-02-25 12:30:22 +0700

    prompt optimization

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -178,9 +178,9 @@ def answer_check(question, input_text, answer_text):
     template = """
     You are an expert at grading student answers. I was given this question: "{question}"
     Your goal is to grade my answer by comparing it to the actual answer using this grading scale:
-        From 0 - 6: Incorrect. You should give out this grading if the my answer is entirely different to the actual answer; has major mistakes (like containing 1 or more points with opposite/contradictive meaning to the actual answer; missing keywords/key points, etc)
+        From 0 - 6: Incorrect. You should give out this grading if the my answer is entirely different to the actual answer, has major mistakes (like containing 1 or more points with opposite/contradictive meaning to the actual answer, missing keywords/key points, etc)
         From 7 - 10: Correct. You should give out this grading if the my answer satisfied all the keypoints of the questions and the actual answer. The phrasing may be different but the answer should be 80-percent-or-above similar to the actual answer. The actual answer and my answer may not have the same examples. You will IGNORE ALL THE EXAMPLES OF THE ACTUAL ANSWER and only focus on the my examples to see if their examples stick to the main points of the question.
-    Remember not to provide the grading and only say Incorrect or Correct.
+    Remember not to provide the grading and only say (Incorrect or Correct).
     
     Here is my answer: "{input_text}"
 
